546c81a0f1f37a41c2cf8ebd341a47664af33815
Rifqiadityaa <rifqiadityaa24@gmail.com>
2023-06-04 02:03:15 +0700

    feat: create new dummy data file

---
+++ src/shared/data/data.js
@@ -0,0 +1,74 @@
+export const imgData = [
+  {
+    id: 1,
+    url: "https://images.unsplash.com/photo-1444491741275-3747c53c99b4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80",
+    title: "Bike in nature",
+    description: "",
+    category: ["Sports", "Cycling", "bicycle"],
+  },
+  {
+    id: 2,
+    url: "https://images.unsplash.com/photo-1530549387789-4c1017266635?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fHNwb3J0fGVufDB8fDB8fHww&w=1000&q=80",
+    title: "",
+    description: "",
+    category: ["Sports", "Swimming"],
+  },
+  {
+    id: 3,
+    url: "https://images.unsplash.com/photo-1484723091739-30a097e8f929?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80",
+    title: "",
+    description: "",
+    category: ["Foods", "Breakfast", "French Toast"],
+  },
+  {
+    id: 4,
+    url: "https://images.unsplash.com/photo-1537824598505-99ee03483384?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80",
+    title: "Lago di Braies (Wildsee Pragser)",
+    description: "",
+    category: ["Scenery", "Nature"],
+  },
+  {
+    id: 5,
+    url: "https://images.unsplash.com/photo-1467803738586-46b7eb7b16a1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80",
+    title: "Metropolitan skyline",
+    description: "",
+    category: ["Skyline", "Skyscraper", "Tower", "Architecture"],
+  },
+  {
+    id: 6,
+    url: "https://images.unsplash.com/photo-1605559424843-9e4c228bf1c2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80",
+    title: "",
+    description: "",
+    category: ["Car", "Automotive", "Mercedes", "Transportation"],
+  },
+  {
+    id: 7,
+    url: "https://images.unsplash.com/photo-1533174072545-7a4b6ad7a6c3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80",
+    title: "Flume at Field Day Sydney",
+    description: "",
+    category: ["Festival", "Concert", "Music", "Audience"],
+  },
+  {
+    id: 8,
+    url: "https://images.unsplash.com/photo-1567620905732-2d1ec7ab7445?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80",
+    title: "Pancake Stack",
+    description: "",
+    category: ["Foods", "Pancake"],
+  },
+  {
+    id: 9,
+    url: "https://images.unsplash.com/photo-1685570103029-b4742961df08?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80",
+    title: "Train on a low shutter speed",
+    description: "",
+    category: ["Train", "Metro", "Railway"],
+  },
+  {
+    id: 10,
+    url: "https://images.unsplash.com/photo-1685282338826-37cd0ea93093?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1000&q=80",
+    title: "Parked",
+    description: "",
+    category: ["Digital Image", "Render"],
+  },
+];
+
+export const categoryData = ["All", "Automotive", "Foods", "Scenery", "Sports"];
